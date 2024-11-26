bc9639637728d60bbbfc051cd71cee430ae225de
bachhungcb <bachhungcb@gmail.com>
2024-11-26 16:22:15 +0700

    create task Controller with simple CRUD

---
+++ src/modules/education-service/tasks/tasks.controller.ts
@@ -0,0 +1,69 @@
+import {Permission} from '@n-constants';
+import {AuthClaims, Roles} from '@n-decorators';
+import {PaginationParamsDto} from '@n-dtos';
+import {Body, Controller, Delete, Get, Param, Patch, Post, Query, UseGuards} from '@nestjs/common';
+import {ApiCreatedResponse, ApiOkResponse, ApiTags} from '@nestjs/swagger';
+
+import {CategoryEntity} from "@n-modules/education-service/tasks/entities/category.entity";
+import {CreateTaskDto} from './dto/create-task.dto';
+import {UpdateTaskDto} from './dto/update-task.dto';
+import {TasksService} from "@n-modules/education-service/tasks/tasks.service";
+
+@Controller('tasks')
+@ApiTags('Tasks')
+export class TasksController {
+    constructor(private readonly tasksService: TasksService) {
+    }
+
+    @Post()
+    @Roles([Permission.CREATE_CATEGORY])
+    @AuthClaims()
+    @ApiCreatedResponse({type: CategoryEntity})
+    create(
+        @Body() createTaskDto: CreateTaskDto,
+    ) {
+        return this.tasksService.createTask(createTaskDto);
+    }
+
+    @Get('pagination')
+    @Roles([Permission.GET_CATEGORIES])
+    @AuthClaims()
+    @ApiOkResponse({
+        type: CategoryEntity,
+        isArray: true
+    })
+    findAll(
+        @Query() {
+            page,
+            pageSize
+        }: PaginationParamsDto,
+    ) {
+        return this.tasksService.getListTask(
+            page,
+            pageSize);
+    }
+
+    @Get(':id')
+    @Roles([Permission.GET_CATEGORY])
+    @AuthClaims()
+    @ApiOkResponse({type: CategoryEntity})
+    findOne(@Param('id') id: string) {
+        return this.tasksService.getTaskById(id);
+    }
+
+    @Patch(':id')
+    @Roles([Permission.UPDATE_CATEGORY])
+    @AuthClaims()
+    @ApiOkResponse({type: CategoryEntity})
+    update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
+        return this.tasksService.updateTask(id, updateTaskDto);
+    }
+
+    @Delete(':id')
+    @Roles([Permission.UPDATE_CATEGORY])
+    @AuthClaims()
+    @ApiOkResponse({type: CategoryEntity})
+    remove(@Param('id') id: string) {
+        return this.tasksService.deleteTask(id);
+    }
+}
