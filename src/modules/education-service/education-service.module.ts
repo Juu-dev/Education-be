a6957c78d2485503496693468c53af95496b88a8
bachhungcb <bachhungcb@gmail.com>
2024-11-26 16:21:11 +0700

    add task module to swagger UI

--- src/modules/education-service/education-service.module.ts
+++ src/modules/education-service/education-service.module.ts
@@ -3,9 +3,10 @@ import {DocumentsModule} from './documents/documents.module';
 import {TeachersModule} from './teachers/teachers.module';
 import {ClassesModule} from "@n-modules/education-service/classes/classes.module";
 import {StudentsModule} from "@n-modules/education-service/students/students.module";
+import {TasksModule} from "@n-modules/education-service/tasks/tasks.module";
 
 @Module({
-    imports: [DocumentsModule, TeachersModule, ClassesModule, StudentsModule],
+    imports: [DocumentsModule, TeachersModule, ClassesModule, StudentsModule,TasksModule],
     providers: [],
     controllers: [],
     exports: [],
