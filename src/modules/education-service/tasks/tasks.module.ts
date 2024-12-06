d24f1f49b28306bbb355a6cb7b09e288db26c0d9
bachhungcb <bachhungcb@gmail.com>
2024-12-06 14:36:42 +0700

    format tasks.module'

--- src/modules/education-service/tasks/tasks.module.ts
+++ src/modules/education-service/tasks/tasks.module.ts
@@ -1,15 +1,15 @@
-import {Module} from '@nestjs/common';
-import {PrismaModule} from '@n-database/prisma/prisma.module';
-import {TasksService} from './tasks.service';
-import {TasksController} from "@n-modules/education-service/tasks/tasks.controller";
-import {TasksRepository} from "@n-modules/education-service/tasks/tasks.repository";
-import {StudentsModule} from "@n-modules/education-service/students/students.module";
-import {TeachersModule} from "@n-modules/education-service/teachers/teachers.module";
+import { Module } from '@nestjs/common';
+import { PrismaModule } from '@n-database/prisma/prisma.module';
+import { TasksController } from '@n-modules/education-service/tasks/tasks.controller';
+import { TasksRepository } from '@n-modules/education-service/tasks/tasks.repository';
+import { StudentsModule } from '@n-modules/education-service/students/students.module';
+import { TeachersModule } from '@n-modules/education-service/teachers/teachers.module';
+import { TasksService } from './tasks.service';
 
 @Module({
-    controllers: [TasksController],
-    providers: [TasksService,TasksRepository],
-    imports: [PrismaModule, StudentsModule, TeachersModule],
+  controllers: [TasksController],
+  providers: [TasksService, TasksRepository],
+  imports: [PrismaModule, StudentsModule, TeachersModule],
 })
 export class TasksModule {
 }
