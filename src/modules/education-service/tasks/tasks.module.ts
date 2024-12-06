eedb648961b3560710ecc7d166af5e0130f964a5
bachhungcb <bachhungcb@gmail.com>
2024-12-06 14:32:18 +0700

    add tasks.module

---
+++ src/modules/education-service/tasks/tasks.module.ts
@@ -0,0 +1,15 @@
+import {Module} from '@nestjs/common';
+import {PrismaModule} from '@n-database/prisma/prisma.module';
+import {TasksService} from './tasks.service';
+import {TasksController} from "@n-modules/education-service/tasks/tasks.controller";
+import {TasksRepository} from "@n-modules/education-service/tasks/tasks.repository";
+import {StudentsModule} from "@n-modules/education-service/students/students.module";
+import {TeachersModule} from "@n-modules/education-service/teachers/teachers.module";
+
+@Module({
+    controllers: [TasksController],
+    providers: [TasksService,TasksRepository],
+    imports: [PrismaModule, StudentsModule, TeachersModule],
+})
+export class TasksModule {
+}
