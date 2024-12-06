6b93a86b3d9f9c97bb5359c3080145787c681f56
bachhungcb <bachhungcb@gmail.com>
2024-12-06 14:50:07 +0700

    fix a bug
    change genericRepository to generic.repository

--- src/modules/education-service/tasks/tasks.repository.ts
+++ src/modules/education-service/tasks/tasks.repository.ts
@@ -1,12 +1,12 @@
-import {Injectable} from '@nestjs/common';
-import {Task} from '@prisma/client';
+import { Injectable } from '@nestjs/common';
+import { Task } from '@prisma/client';
 
-import {PrismaService} from '@n-database/prisma/prisma.service';
-import {GenericRepository} from "@n-modules/generic-service/genericRepository";
+import { PrismaService } from '@n-database/prisma/prisma.service';
+import { GenericRepository } from "@n-modules/generic-service/generic.repository";
 
 @Injectable()
 export class TasksRepository extends GenericRepository<Task> {
-    constructor(private readonly prismaService: PrismaService) {
-        super(prismaService, 'task');
-    }
+  constructor(private readonly prismaService: PrismaService) {
+    super(prismaService, 'task');
+  }
 }
--- src/modules/education-service/tasks/tasks.service.ts
+++ src/modules/education-service/tasks/tasks.service.ts
@@ -1,86 +1,84 @@
-import {Injectable, NotFoundException} from '@nestjs/common';
-import {BaseException} from '@n-exceptions';
-import {Errors} from '@n-constants';
-import {CreateTaskDto, UpdateTaskDto} from './dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { BaseException } from '@n-exceptions';
+import { Errors } from '@n-constants';
+import { CreateTaskDto, UpdateTaskDto } from './dto';
 import { TasksRepository } from './tasks.repository';
 import { StudentsService } from '../students/students.service';
-import {TeachersService} from '../teachers/teachers.service';
+import { TeachersService } from '../teachers/teachers.service';
 
 @Injectable()
 export class TasksService {
-    constructor(
-        private readonly tasksRepository: TasksRepository,
-        private readonly studentsService: StudentsService,
-        private readonly teachersService: TeachersService,) {
+  constructor(
+    private readonly tasksRepository: TasksRepository,
+    private readonly studentsService: StudentsService,
+    private readonly teachersService: TeachersService,
+  ) {
+  }
+
+  async createTask(createTaskDto: CreateTaskDto) {
+    //Check if the assignee exists
+    const assigneeExists = await this.teachersService.getTeacherById(createTaskDto.assigneeId);
+    if (!assigneeExists) {
+        console.log(`Assignee with ID ${createTaskDto.assigneeId} does not exist`)
+        throw new NotFoundException(`Assignee with ID ${createTaskDto.assigneeId} does not exist`);
     }
 
-
-    async createTask(createTaskDto: CreateTaskDto) {
-        // Check if the assignee exists
-        // const assigneeExists = await this.teachersService.getTeacherById(createTaskDto.assigneeId);
-        // if (!assigneeExists) {
-        //     console.log(`Assignee with ID ${createTaskDto.assigneeId} does not exist`)
-        //     throw new NotFoundException(`Assignee with ID ${createTaskDto.assigneeId} does not exist`);
-        // }
-        //
-        // // Check if the assigner exists
-        // const assignerExists = await this.studentsService.getStudentById(createTaskDto.assignerId) //await this.tasksRepository.findUserById(createTaskDto.assignerId);
-        // if (!assignerExists) {
-        //     throw new NotFoundException(`Assigner with ID ${createTaskDto.assignerId} does not exist`);
-        // }
-
-        return this.tasksRepository.create(createTaskDto as any)
+    // Check if the assigner exists
+    const assignerExists = await this.studentsService.getStudentById(createTaskDto.assignerId) //await this.tasksRepository.findUserById(createTaskDto.assignerId);
+    if (!assignerExists) {
+        throw new NotFoundException(`Assigner with ID ${createTaskDto.assignerId} does not exist`);
     }
 
-    async getListTask(
-        page?: number,
-        pageSize?: number,
-    ) {
-        const count = await this.tasksRepository.count();
+    return this.tasksRepository.create(createTaskDto as any);
+  }
 
-        const items = await this.tasksRepository.findAllPagination(page, pageSize);
+  async getListTask(
+    page?: number,
+    pageSize?: number,
+  ) {
+    const count = await this.tasksRepository.count();
 
-        return {
-            pagination: {
-                page: page,
-                pageSize: pageSize,
-                totalPage: Math.ceil(count / pageSize)
-            },
-            count,
-            data: items,
-        };
+    const items = await this.tasksRepository.findAllPagination(page, pageSize);
+
+    return {
+      pagination: {
+        page,
+        pageSize,
+        totalPage: Math.ceil(count / pageSize),
+      },
+      count,
+      data: items,
+    };
+  }
+
+  async getTaskById(id: string) {
+    const task = await this.tasksRepository.findById(id);
+
+    if (!task) {
+      throw new BaseException(Errors.CATEGORY.CATEGORY_NOT_FOUND);
     }
 
-    async getTaskById(id: string) {
-        const task = await this.tasksRepository.findById(id);
+    return task;
+  }
 
-        if (!task) {
-            throw new BaseException(Errors.CATEGORY.CATEGORY_NOT_FOUND);
-        }
-
-        return task;
+  async updateTask(id: string, UpdateTaskDto: UpdateTaskDto) {
+    const task = await this.tasksRepository.findById(id);
+    if (!task) {
+      throw new BaseException(Errors.CATEGORY.CATEGORY_NOT_FOUND);
     }
 
-    async updateTask(id: string, UpdateTaskDto: UpdateTaskDto) {
-        const task = await this.tasksRepository.findById(id);
-        if (!task) {
-            throw new BaseException(Errors.CATEGORY.CATEGORY_NOT_FOUND);
-        }
+    return this.tasksRepository.updateById(
+      id,
+      UpdateTaskDto as any,
+    );
+  }
 
-        return this.tasksRepository.updateById(
-            id,
-            UpdateTaskDto as any,
-        );
-
+  async deleteTask(id: string) {
+    const task = await this.tasksRepository.findById(id);
+    if (!task) {
+      throw new BaseException(Errors.CATEGORY.CATEGORY_NOT_FOUND);
     }
 
-    async deleteTask(id: string) {
-        const task = await this.tasksRepository.findById(id);
-        if (!task) {
-            throw new BaseException(Errors.CATEGORY.CATEGORY_NOT_FOUND);
-        }
-
-        return this.tasksRepository.deleteById(id);
-    }
-
+    return this.tasksRepository.deleteById(id);
+  }
 }
